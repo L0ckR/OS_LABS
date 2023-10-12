4f83726e1c5b3b33ba561cefdb2e949ad8920aab
lockr <doly2004e@yandex.ru>
2023-10-12 14:19:35 +0300

    using pthread

--- lab2/include/monte-carlo.hpp
+++ lab2/include/monte-carlo.hpp
@@ -3,13 +3,19 @@
 
 #include <iostream>
 #include <vector>
-#include <thread>
+#include <pthread.h>
 #include <atomic>
 
 #include <cmath>
 #include <ctime>
 
 #define _USE_MATH_DEFINES
+
+typedef struct args{
+    double radius = 0;
+    size_t dotsPerThread = 0;
+    unsigned * seed = nullptr;
+} Args;
 
 typedef struct{
     double x = 0;
@@ -20,6 +26,6 @@ bool IsInCircle(const Coordinats &cords, const double radius);
 
 Coordinats CreateRandCoord(const double radius, unsigned int * seed);
 
-void task(const double radius, std::atomic<size_t> &dotsInCircle, const size_t dotsPerThread, unsigned int * seed);
+void *task(void *input);
 
 double CircleArea(const size_t threadQuantity, const double radius, const size_t dotsQuantity);
--- lab2/src/monte-carlo.cpp
+++ lab2/src/monte-carlo.cpp
@@ -1,5 +1,7 @@
 #include <monte-carlo.hpp>
 
+std::atomic<size_t> dotsInCircle {0};
+
 bool IsInCircle(const Coordinats &cords, const double radius)
 {
     // if ((cords.x > radius) || (cords.y > radius)){
@@ -8,7 +10,7 @@ bool IsInCircle(const Coordinats &cords, const double radius)
     return (cords.x * cords.x + cords.y * cords.y) <= radius * radius;
 }
 
-Coordinats CreateRandCoord(const double radius, unsigned int * seed)
+Coordinats CreateRandCoord(const double radius, unsigned int *seed)
 {
     const Coordinats cords = {
         (double)rand_r(seed) / (RAND_MAX / radius),
@@ -16,64 +18,69 @@ Coordinats CreateRandCoord(const double radius, unsigned int * seed)
     return cords;
 }
 
-void task(const double radius, std::atomic<size_t> &dotsInCircle, const size_t dotsPerThread, unsigned int *seed)
+void *task(void *input)
 {
     size_t counter = 0;
-    for (size_t i = 0; i < dotsPerThread; i++)
+    for (size_t i = 0; i < (reinterpret_cast<Args *>(input))->dotsPerThread; i++)
     {
-        if (IsInCircle(CreateRandCoord(radius, seed), radius))
+        if (IsInCircle(CreateRandCoord((reinterpret_cast<Args *>(input))->radius, (reinterpret_cast<Args *>(input))->seed), (reinterpret_cast<Args *>(input))->radius))
         {
             counter++;
         }
     }
+    delete (reinterpret_cast<Args *>(input))->seed;
     dotsInCircle.fetch_add(counter, std::memory_order_relaxed);
+    pthread_exit(0);
 }
 
 double CircleArea(const size_t threadQuantity, const double radius, const size_t dotsQuantity)
 {
-
+    double result = 0;
     if (threadQuantity >= 1)
     {
         time_t currentTime = time(nullptr);
-        std::atomic<size_t> dotsInCircle{0};
         const size_t actualThreadQuantity = std::min(threadQuantity, dotsQuantity);
-        std::vector<std::thread> threads;
-        threads.reserve(actualThreadQuantity);
+        std::vector<pthread_t> threads(actualThreadQuantity);
 
         const size_t dotsPerThread = dotsQuantity / actualThreadQuantity;
 
-        for (size_t i = 0; i < dotsQuantity; i += dotsPerThread)
-        {
-            unsigned int seed =  currentTime + (i % dotsPerThread);
-            if (i + dotsPerThread >= dotsQuantity)
+        std::vector<Args> argsForThread(actualThreadQuantity);
+
+        for (size_t i = 0; i < actualThreadQuantity; i++)
+        {   
+            argsForThread[i].radius = radius;
+            argsForThread[i].dotsPerThread = dotsPerThread;
+            argsForThread[i].seed = new unsigned int;
+            *argsForThread[i].seed = currentTime + i;
+            if ((i + 1) * dotsPerThread >= dotsQuantity)
             {
-                size_t dotsPerLastThread = dotsQuantity - i;
-                threads.emplace_back(task, std::ref(radius), std::ref(dotsInCircle), std::ref(dotsPerLastThread), &seed);
+                argsForThread[i].dotsPerThread = dotsQuantity - i * dotsPerThread;
+                pthread_create(&threads[i], nullptr, task, reinterpret_cast<void *>(&argsForThread[i]));
             }
             else
             {
-                threads.emplace_back(task, std::ref(radius), std::ref(dotsInCircle), std::ref(dotsPerThread), &seed);
+                pthread_create(&threads[i], nullptr, task, reinterpret_cast<void *>(&argsForThread[i]));
             }
         }
 
         for (auto &thread : threads)
         {
-            thread.join();
+            pthread_join(thread, nullptr);
         }
-
-        return ((2 * radius) * (2 * radius)) * ((double)dotsInCircle / dotsQuantity);
+        result = ((double)dotsInCircle / dotsQuantity);
     }
     else
     {
-        size_t dotsInCircle = 0;
+        size_t dotsInCircleSingle = 0;
         unsigned int seed = (unsigned)time(nullptr);
         for (size_t i = 0; i < dotsQuantity; i++)
         {
             if (IsInCircle(CreateRandCoord(radius, &seed), radius))
             {
-                dotsInCircle++;
+                dotsInCircleSingle++;
             }
         }
-        return ((2 * radius) * (2 * radius)) * ((double)dotsInCircle / dotsQuantity);
+        result = ((double)dotsInCircleSingle / dotsQuantity);
     }
+    return ((2 * radius) * (2 * radius)) * result;
 }
